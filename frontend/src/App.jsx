15045545f543a30d42c59e7448d5ecdd0ee3a805
Shindie <ntiendung04@gmail.com>
2025-12-17 23:50:31 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    fetch(import.meta.env.VITE_API_URL)
+    fetch(import.meta.env.VITE_API_URL || 'https://localhost:8081')
       .then(res => res.json())
       .then(data => setMsg(data.message));
   }, []);
 
-  return <h1>{msg}</h1>;
+  return <>
+    <h1>Hello</h1>
+    <h1>{msg}</h1>
+  </>
 }
 
 export default App;
