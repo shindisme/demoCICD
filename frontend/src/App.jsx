eafde88af8acab21632d3f73f0b235c9b56a4c75
Shindie <ntiendung04@gmail.com>
2025-12-18 00:20:18 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [msg, setMsg] = useState("");
+  const [message, setMessage] = useState("Loading...");
+  const [time, setTime] = useState("");
 
   useEffect(() => {
-    fetch(import.meta.env.VITE_API_URL || 'https://localhost:8081')
+    fetch(import.meta.env.VITE_API_URL || 'https://democicd-acln.onrender.com')
       .then(res => res.json())
-      .then(data => setMsg(data.message));
+      .then(data => {
+        setMessage(data.message);
+        setTime(data.time);
+      })
+      .catch(() => {
+        setMessage("❌ Cannot connect to backend");
+      });
   }, []);
 
-  return <>
-    <h1>Hello</h1>
-    <h1>{msg}</h1>
-  </>
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>CI/CD DEMO 🚀</h1>
+
+      <p><b>Message from backend:</b></p>
+      <h2>{message}</h2>
+
+      <p><b>Server time:</b></p>
+      <h3>{time}</h3>
+
+      <hr />
+    </div>
+  );
 }
 
 export default App;
